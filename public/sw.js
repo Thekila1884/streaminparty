2165fe87603026029f9de5c1e2e3adc466d897aa
Thekila1884 <Juniorcede3002@gmail.com>
2026-09-07 16:33:15 -0500

    fix: invalidate stale service worker cache

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "streaminparty-shell-v2";
+const CACHE_NAME = "streaminparty-shell-v3";
 const APP_SHELL = ["/", "/index.html", "/manifest.webmanifest"];
 
 self.addEventListener("install", (event) => {
